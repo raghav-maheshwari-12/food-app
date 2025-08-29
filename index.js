df27a35f51d5a1090aff7b48fa2e53f6a003a14d
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-08-29 22:59:33 +0530

    user route path added

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(morgan("dev"));
 
 app.use("/api/v1/test", require("./routes/testRoute"));
 app.use("/api/v1/auth", require("./routes/authRoute"));
+app.use("/api/v1/user", require("./routes/userRoute"));
 
 app.get("/", (req, res) => {
   return res
