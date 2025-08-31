b548ad9d3240c956064fc12f2e585a1ae836d689
raghav_maheshwari_12 <darakraghav90@gmail.com>
2025-08-31 18:36:29 +0530

    category route added

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use("/api/v1/test", require("./routes/testRoute"));
 app.use("/api/v1/auth", require("./routes/authRoute"));
 app.use("/api/v1/user", require("./routes/userRoute"));
 app.use("/api/v1/resturant", require("./routes/resturantRoute"));
+app.use("/api/v1/category", require("./routes/categoryRoute"));
 
 app.get("/", (req, res) => {
   return res
